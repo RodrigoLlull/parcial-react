50ac7e44f61c71642aa936478659d59dd8d18a0f
Rodrigo Llull <rodrigollullrodriguez@gmail.com>
2024-08-12 21:19:17 -0300

    Estructura del parcial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './modules/card'
+import Form from './modules/form'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <Form />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/modules/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>Hola soy una tarjeta</div>
+  )
+}
+
+export default card
